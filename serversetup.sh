c8b3ac43289eb1006c3160d21588a0a66ca5ae3a
dropitlikecross <44788043+dropitlikecross@users.noreply.github.com>
2018-11-06 07:00:08 +0900

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,16 +1,16 @@
-apt-ge update
-apt-get install screen nano bzip2 mpg123 htop git
-apt-get install autoconf automake build-essential libass-dev libfreetype6-dev libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev
-apt-get install librtmp-dev libssl-dev libx264-dev libasound2-dev
-apt-get install libomxil-bellagio-dev
+apt-get update
+apt-get -y install screen nano bzip2 mpg123 htop git
+apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev
+apt-get -y install librtmp-dev libssl-dev libx264-dev libasound2-dev
+apt-get -y install libomxil-bellagio-dev
 
-apt-get install libfdk-aac-dev
+apt-get -y install libfdk-aac-dev
 
-apt-get install ffmpeg
+apt-get -y install ffmpeg
 
 ffmpeg -v
 
-apt-get install linux-generic
+apt-get -y install linux-generic
 
 modprobe snd-aloop pcm_substreams=1
 
