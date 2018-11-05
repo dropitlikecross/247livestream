dc112b4e428a7bddce5e2addf75d9e8179e2d16d
dropitlikecross <44788043+dropitlikecross@users.noreply.github.com>
2018-11-06 06:58:30 +0900

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,3 +1,4 @@
+apt-ge update
 apt-get install screen nano bzip2 mpg123 htop git
 apt-get install autoconf automake build-essential libass-dev libfreetype6-dev libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev
 apt-get install librtmp-dev libssl-dev libx264-dev libasound2-dev
