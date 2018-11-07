e6b60b716bbb9ad7a1d9decbbdc61c1bca45bfcf
dropitlikecross <44788043+dropitlikecross@users.noreply.github.com>
2018-11-07 22:07:48 +0900

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -11,9 +11,10 @@ COLOR="0xFFFFFF"
 BCOLOR="0x000000"
 
 
-$FFMPEG -f alsa -ac 2 -i hw:Loopback,1,0 -fflags +genpts -ignore_loop 0 -r $FPS -i "$GIF"  \
--vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:bordercolor=$BCOLOR:borderw=1:fontcolor=$COLOR:textfile=$TEXT:reload=1:y=h-line_h-20:x=10" 
--vcodec h264_omx -x264opts keyint=$KEYINT:min-keyint=$KEYINT:scenecut=-1 -b:v 1000k \
--preset veryfast -pix_fmt yuv420p -s 854x480 \
+$FFMPEG -f alsa -ac 2 -i hw:Loopback,1,0 -fflags +genpts -ignore_loop 0 -r $FPS -i $GIF \
+-vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:bordercolor=$BCOLOR: borderw=1: fontcolor=$COLOR:textfile=$TEXT:reload=1:y=10:x=5" \
+-vcodec libx264 -b:v 1000k \
+-preset veryfast -pix_fmt yuv420p -s 1920x1080 \
 -c:a libfdk_aac -b:a 96k -ar 48000 \
 -f flv $URL
+
