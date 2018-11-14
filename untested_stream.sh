11dd6623679fa80bd085caf8aea3af74407750e7
Cross <44788043+dropitlikecross@users.noreply.github.com>
2018-11-15 06:12:45 +0900

    Update untested_stream.sh

--- untested_stream.sh
+++ untested_stream.sh
@@ -1,17 +1,25 @@
-#variable definitions
-GIF=/your/animated_gif.gif
-INRES="1366x768" # input resolution
-OUTRES="1366x768" # output resolution
-FPS="15" # target FPS
-GOP="30" # i-frame interval, should be double of FPS,
-GOPMIN="15" # min i-frame interval, should be equal to fps,
-THREADS="2" # max 6
-CBR="1100k" # constant bitrate (should be between 1000k - 3000k)
-QUALITY="veryfast" # one of the many FFMPEG preset
-AUDIO_RATE="44100"
-STREAM_KEY="" # your streaming key goes here
-#to hide logs use= -loglevel quiet
-ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i $GIF -i :0.0 -f alsa -i hw:Loopback,1,0 -f flv -ac 2 -ar $AUDIO_RATE \
--vcodec libx264 -keyint_min 3 -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p \
--s $OUTRES -preset $QUALITY -acodec aac -threads $THREADS \
--bufsize $CBR "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
+#!/bin/bash
+
+sudo modprobe snd-aloop pcm_substreams=1
+FFMPEG=ffmpeg
+GIF=/mnt/streamstorage/video/gamingstream/narutocity.gif
+STREAM_KEY=
+TEXT=/tmp/current_song.txt
+COLOR="0xFFFFFF"
+BCOLOR="0x000000"
+INRES=1366x768 # input resolution
+OUTRES=1366x768 # output resolution
+FPS=15 # target FPS
+GOP=30 # i-frame interval, should be double of FPS,
+GOPMIN=15 # min i-frame interval, should be equal to fps,
+THREADS=2 # max 6
+CBR=1100k # constant bitrate (should be between 1000k - 3000k)
+QUALITY=veryfast # one of the many FFMPEG preset
+AUDIO_RATE=44100
+KEYINT=$(expr $FPS \* 3)
+
+$FFMPEG -thread_queue_size 1024 -f alsa -ac 2 -i hw:Loopback,1,0 -fflags +genpts -ignore_loop 0 -r $FPS -i $GIF \
+-vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:bordercolor=$BCOLOR: borderw=1: fontcolor=$COLOR:textfile=$TEXT:reload=1:y=10:x=5" \
+-vcodec libx264 -x264opts keyint=$KEYINT:min-keyint=$KEYINT:scenecut=-1 -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p \
+-s $OUTRES -preset $QUALITY -acodec aac -threads $THREADS -fflags nobuffer \
+-bufsize $CBR -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
