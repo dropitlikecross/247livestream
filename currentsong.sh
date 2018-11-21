be183c353d52627019af0787c6787f1b98f5939d
Cross <44788043+dropitlikecross@users.noreply.github.com>
2018-11-21 21:39:20 +0900

    Update currentsong.sh

--- currentsong.sh
+++ currentsong.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-while [ true ]
+while true
 do
   mpc current > current_song.txt
-        if [ ! -s current_song.txt ]; then echo Subscribe > current_song.txt; fi
+  if [ ! -s current_song.txt ]; then
+    echo Subscribe > current_song.txt
+  fi
   mpc idle player
 done
