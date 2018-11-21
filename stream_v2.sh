ed4f2c1addbeeea76d61dfa91f6478e8eb57b8ca
Cross <44788043+dropitlikecross@users.noreply.github.com>
2018-11-21 21:48:45 +0900

    Update stream_v2.sh

--- stream_v2.sh
+++ stream_v2.sh
@@ -18,7 +18,7 @@ AUDIO_RATE=44100
 KEYINT=$(expr $FPS \* 3)
 
 $FFMPEG -thread_queue_size 1024 -f alsa -ac 2 -i hw:Loopback,1,0 -fflags +genpts -ignore_loop 0 -r $FPS -i $GIF \
--vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:bordercolor=$BCOLOR: borderw=1: fontcolor=$COLOR:textfile=$TEXT:reload=1:y=10:x=5" \
+-vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:bordercolor=$BCOLOR: borderw=1: fontcolor=$COLOR:textfile=$TEXT:reload=1:y=10:x=5:fontsize=32" \
 -vcodec libx264 -x264opts keyint=$KEYINT:min-keyint=$KEYINT:scenecut=-1 -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p \
 -s $OUTRES -preset $QUALITY -acodec aac -threads $THREADS -fflags nobuffer \
 -bufsize $CBR -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY" \
