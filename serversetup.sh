cc9180f46f550baeff0ba15ab5fe4999d0448903
Cross <44788043+dropitlikecross@users.noreply.github.com>
2019-02-09 07:47:00 +0900

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -4,6 +4,13 @@ apt-get update
 apt-get -y install screen nano bzip2 mpc mpd htop git
 apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev
 apt-get -y install librtmp-dev libssl-dev libx264-dev libasound2-dev
+
+sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+
+sudo apt-get update
+
+sudo apt-get -y install ffmpeg
+
 apt-get -y install libomxil-bellagio-dev
 
 apt-get -y install libfdk-aac-dev
@@ -19,3 +26,5 @@ apt-get -y install linux-generic
 echo 'pcm.!default { type plug slave.pcm "hw:Loopback,0,0" }' >> ~/.asoundrc
 
 ldconfig
+
+
